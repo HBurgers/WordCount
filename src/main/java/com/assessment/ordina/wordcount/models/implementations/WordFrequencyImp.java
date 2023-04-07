3608c871db932f3a58aeddac070b53523dc25f05
HBurgers <heinrich.burgers@gmail.com>
2023-04-07 19:07:54 +0200

    Basic implementation. Thus far untested. Needs to be tested either in QA-5 via controller or in QA-6 via unit tests.

--- src/main/java/com/assessment/ordina/wordcount/models/implementations/WordFrequencyImp.java
+++ src/main/java/com/assessment/ordina/wordcount/models/implementations/WordFrequencyImp.java
@@ -2,6 +2,9 @@ package com.assessment.ordina.wordcount.models.implementations;
 
 import com.assessment.ordina.wordcount.models.WordFrequency;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
 
 public class WordFrequencyImp implements WordFrequency {
 
@@ -9,4 +12,30 @@ public class WordFrequencyImp implements WordFrequency {
     private String word;
     @Getter
     private int frequency;
+
+    public WordFrequencyImp(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
+    }
+
+    public void incrementFrequency() {
+        this.frequency++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o instanceof WordFrequencyImp that) {
+            return word.equals(that.word);
+        } else if (o instanceof String that) {
+            return word.equals(that);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(word, frequency);
+    }
 }
--- src/main/java/com/assessment/ordina/wordcount/services/implementations/WordFrequencyAnalyzerService.java
+++ src/main/java/com/assessment/ordina/wordcount/services/implementations/WordFrequencyAnalyzerService.java
@@ -1,12 +1,14 @@
 package com.assessment.ordina.wordcount.services.implementations;
 
 import com.assessment.ordina.wordcount.models.WordFrequency;
+import com.assessment.ordina.wordcount.models.implementations.WordFrequencyImp;
 import com.assessment.ordina.wordcount.services.WordFrequencyAnalyzer;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -15,49 +17,70 @@ public class WordFrequencyAnalyzerService implements WordFrequencyAnalyzer {
     private final String SPLIT_REGEX = "([^a-zA-Z])+";
 
     @Override
-    public int calculateHighestFrequency(String text) {
+    public int calculateHighestFrequency(final String text) {
         if (StringUtils.isBlank(text)) {
+            //No text provided. Highest frequency will thus be 0;
             return 0;
         }
 
-        Optional<Map.Entry<String, Integer>> highestEntry = toCountedMap(text).entrySet().stream().max(Map.Entry.comparingByValue());
-
-        return highestEntry.get();
+        return calculateHighestFrequencyFromList(toSortedWordFrequencyList(text));
     }
 
     @Override
-    public int calculateFrequencyForWord(String text, String word) {
-        return 0;
-    }
+    public int calculateFrequencyForWord(final String text, final String word) {
 
-    @Override
-    public WordFrequency[] calculateMostFrequentNWords(String text, int n) {
-        return new WordFrequency[0];
-    }
-
-//    private List splitText(String text) {
-//        if(StringUtils.isBlank(text)) {
-//            return new ArrayList<>();
-//        }
-//
-//        return Arrays.stream(text.split(SPLIT_REGEX)).filter(word -> StringUtils.isNotBlank(word)).collect(Collectors.toList());
-//    }
-
-    private  Map<String, Integer> toCountedMap(String inputText) {
-        if(StringUtils.isBlank(inputText)) {
-            return Collections.EMPTY_MAP;
+        if (StringUtils.isBlank(text) || StringUtils.isBlank(word)) {
+            //Either text is blank or word is blank. Either way, word does not appear in text thus return 0
+            return 0;
         }
 
-        HashMap<String, Integer> countedMap = new HashMap<>();
+        final List<WordFrequencyImp> frequencyList = toSortedWordFrequencyList(text);
 
-        Arrays.stream(inputText.split(SPLIT_REGEX)).map(String::toUpperCase).forEach(filteredWord -> {
-            if (countedMap.containsKey(filteredWord)) {
-                countedMap.replace(filteredWord, countedMap.get(filteredWord) + 1);
-            } else {
-                countedMap.put(filteredWord, 1);
-            }
-        });
-
-        return countedMap;
+        return frequencyList.contains(word) ? frequencyList.get(frequencyList.indexOf(word)).getFrequency() : 0;
     }
+
+    @Override
+    public WordFrequency[] calculateMostFrequentNWords(final String text, final int n) {
+
+        if (StringUtils.isBlank(text) || n < 0) {
+            //Either no text has been provided or an invalid value for n is provided.
+            return new WordFrequency[0];
+        }
+
+        return mapNElementsToWordFrequencyArray(toSortedWordFrequencyList(text), n);
+    }
+
+    private List<WordFrequencyImp> toSortedWordFrequencyList(String inputText) {
+        if (StringUtils.isBlank(inputText)) {
+            return new ArrayList<>();
+        }
+
+        List<WordFrequencyImp> wordFrequencyList = new ArrayList<>();
+
+        Arrays.stream(inputText.split(SPLIT_REGEX))
+                .filter(StringUtils::isNotBlank)
+                .map(String::toLowerCase)
+                .forEach(filteredWord -> {
+                    if (wordFrequencyList.contains(filteredWord)) {
+                        wordFrequencyList.get(wordFrequencyList.indexOf(filteredWord)).incrementFrequency();
+                    } else {
+                        wordFrequencyList.add(new WordFrequencyImp(filteredWord, 1));
+                    }
+                });
+
+        return wordFrequencyList.stream().sorted((o1, o2) -> Integer.compare(o2.getFrequency(), o1.getFrequency())).collect(Collectors.toList());
+    }
+
+    private int calculateHighestFrequencyFromList(final List<WordFrequencyImp> frequencyList) {
+        return frequencyList.isEmpty() ? 0 : frequencyList.get(0).getFrequency();
+    }
+
+    private WordFrequency[] mapNElementsToWordFrequencyArray(final List<WordFrequencyImp> wordFrequencyList, final int n) {
+        if (n >= wordFrequencyList.size()) {
+            return wordFrequencyList.toArray(WordFrequency[]::new);
+        }
+
+        return wordFrequencyList.stream().limit(n).toArray(WordFrequency[]::new);
+    }
+
 }
